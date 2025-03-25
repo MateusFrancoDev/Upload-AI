ff4f751d1f235ebd6c3dd70708d0beee5ccb9b07
Mateus Franco <112667339+MateusFrancoDev@users.noreply.github.com>
2025-03-25 19:25:43 -0300

    Mudanças no Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"]
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 })
