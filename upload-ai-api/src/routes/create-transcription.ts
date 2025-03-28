7188ce84c59dac1384d6cdc24da094a30120ca12
Mateus Franco <112667339+MateusFrancoDev@users.noreply.github.com>
2025-03-27 21:28:15 -0300

    Finalizado a parte do back-end

--- upload-ai-api/src/routes/create-transcription.ts
+++ upload-ai-api/src/routes/create-transcription.ts
@@ -1,22 +1,22 @@
-import { FastifyInstance } from "fastify";
 import { createReadStream } from "node:fs";
+import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-  app.post("/videos/:videoId/transcription", async (req) => {
+  app.post("/videos/:videoId/transcription", async (request, reply) => {
     const paramsSchema = z.object({
       videoId: z.string().uuid(),
     });
 
-    const { videoId } = paramsSchema.parse(req.params);
+    const { videoId } = paramsSchema.parse(request.params);
 
     const bodySchema = z.object({
       prompt: z.string(),
     });
 
-    const { prompt } = bodySchema.parse(req.body);
+    const { prompt } = bodySchema.parse(request.body);
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -29,16 +29,26 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 
     const response = await openai.audio.transcriptions.create({
       file: audioReadStream,
-      model: "whisper-1",
+      model: "gpt-4o-transcribe",
       language: "pt",
       response_format: "json",
       temperature: 0,
+      prompt,
+    });
+
+    const transcription = response.text;
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription,
+      },
     });
 
     return {
-      videoId,
-      prompt,
-      videoPath,
+      transcription,
     };
   });
 }
